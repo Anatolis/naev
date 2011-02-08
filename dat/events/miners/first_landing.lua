72e4e075fff97a42dea994c1282b66941eb5d68b
Anatolis <anatolis.bsw@gmail.com>
2011-02-08 21:19:05 +0100

    Added new miner mission
    Actually an event on first-time landing

--- dat/events/miners/first_landing.lua
+++ dat/events/miners/first_landing.lua
@@ -1,6 +1,6 @@
 --[[	
 	+ First landing
-		- Just a simple message to the player, welcoming him on his first arival.
+		- Just a simple message to the player, welcoming him on his first arival in the system.
 		- More missions are to be presented in the bar.
 
 		- Programmed: Anatolis (feb 2011)
@@ -12,14 +12,17 @@ if lang == "es" then
 else -- default english
    
     title = "First arival"
-    text = [["Welcome to our new miningbase %s! My name is <name> I am glad you made it here. As you can see your first mission for us was quite succesfull. However, we are desperate for some other stuff before we are fully opperational. We would be very happy if you could help us out again. If you have time, please visit me at the bar and we have a drink while chatting."
-
-You see <name> walk away and you can't help yourself watching to the extrodenary structure of the spacestation of which you jettisonned the modules only a number of STPs ago.]]
+    text = [[ 
+As you enter the system you see a brand new station appearing on you radar. ]]
 end
 
 function create () 
 	-- Upon landing the text is shown.
+	hook.timer( 2000, "showMessage" )
+	
+end
+
+function showMessage ()
 	tk.msg(title, text:format( player.name() ) )
 	var.push( "miner_stat", 2 )
 end
-
