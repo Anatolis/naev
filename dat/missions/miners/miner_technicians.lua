6d4ded236089081ce2472634f4e2c581e626f041
Anatolis <anatolis.bsw@gmail.com>
2011-02-16 22:12:21 +0100

    Added NPC name and changed pickup location

--- dat/missions/miners/miner_technicians.lua
+++ dat/missions/miners/miner_technicians.lua
@@ -12,7 +12,7 @@ if lang == "es" then
    -- not translated atm
 else -- default English
    -- Mission details
-   bar_desc = "Surprisingly you see miner <X> sitting at the bar. You would not have expected him here."
+   bar_desc = "Surprisingly you see miner Gallain sitting at the bar. You would not have expected him here."
    misn_title = "First miners"
    misn_reward = "%d credits"
    misn_desc = {}
@@ -21,7 +21,7 @@ else -- default English
 
    -- Fancy text messages
    title = {}
-   title[1] = "Miner <X>"
+   title[1] = "Miner Gallain"
    title[2] = "Accepted"
    title[2] = "The miners"
    title[3] = "Embarking"
@@ -38,7 +38,7 @@ Good luck!"]]
 	text[4] = [[Once you are docked with the space station the miners jumped in their space suits. They leave your ship via the airlock. From you cockpit you see them walking in different directions. Only a few minutes later the lights in the station start flickering until they keep on burning steadily. 
 	One of the miners approaches your ship, this time without his spacesuit. "Thank you for your help. All systems function normally and the atmosphere is breathable, so if you want you can leave your ship." He hands you a creditchip. 
 	
-	"If you want we have some more missions for you. When you're interested please visit the bar."]]
+"If you want we have some more missions for you. When you're interested please visit the bar."]]
 
    -- Errors
    errtitle = {}
@@ -47,7 +47,7 @@ Good luck!"]]
    errtitle[3] = "Enough!"
    err = {}
    err[1] = "You do not have enough space to load the packages. You need to make room for %d more tons."
-   err[2] = [[ "Too bad. I hoped you had time right now. Perhaps we will meet again in the future." 
+   err[2] = [["Too bad. I hoped you had time right now. Perhaps we will meet again in the future." 
 <X> turn around an walks away, clearly disappointed. ]]
    err[3] = [[The miners make you just mad with their chatter and loud noises. You lock your cockpit and open the main cargo door, venting the miners and their equipment into space. That will teach them! ]]
    err[4] = [[You just don't have time any more. You ask the miners nicely to leave your ship.]] 
@@ -57,7 +57,7 @@ end
 
 function create ()
    -- Note: this mission does not make any system claims.
-   misn.setNPC( "Miner <x>", "none" )
+   misn.setNPC( "Miner Gallain", "none" )
    misn.setDesc( bar_desc )
    inspace = false -- Just a tracker for a fun message on abortion.
 end
@@ -75,7 +75,7 @@ function accept ()
 	var.push( "miner_status", 2 ) -- Next stage in the miner missions. 2 or higher allows landing on base
 	
 	-- target destination
-	pickup,pickupsys = planet.get( "Selphod" ) -- TODO: random location picking
+	pickup,pickupsys = planet.get( "Caladan" ) -- TODO: random location picking
 	dest,destsys = planet.get( "Mining base Alpha" )
    
 	misn_marker = misn.markerAdd( pickupsys, "low" )
